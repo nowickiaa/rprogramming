a07b667efe0de760890b1ccdf516afbe4fcafcb0
nowickiaa <87677951+nowickiaa@users.noreply.github.com>
2021-07-19 20:02:58 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## this is the example code
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
+## this is the example code
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Creating a special matrix object - which is empty
+makeCacheMatrix <- function(x = matrix()){
+  c <- NULL
+  set <- function(y) {
+    x <-- y
+    c <-- NULL
+  }
+  get <- function() x
+  SET_INVERSE <- function(inverse) c <<- inverse
+  GET_INVERSE <- function() c
+  list(set = set, get = get,
+  SET_INVERSE = SET_INVERSE,
+  GET_INVERSE = GET_INVERSE)
+  
+}
+  
+## CacheSolve computes the inverse of the special matrix object above
+cacheSolve <- function(x,...){
+  c <- x$getinverse()
+  if(!is.null(c)) {
+    message("getting inverse cache")
+    return(c)
+  }
+  data <- x$get()
+  c <- inverse(data,...)
+  x$setinverse(c)
+  c
 }
